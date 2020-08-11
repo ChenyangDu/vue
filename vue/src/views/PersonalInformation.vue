cb465583a8a2887d582a84a546620df5e7f20555
1821dph <63182060+1821dph@users.noreply.github.com>
2020-08-11 15:13:13 +0800

    Update PersonalInformation.vue

--- vue/src/views/PersonalInformation.vue
+++ vue/src/views/PersonalInformation.vue
@@ -1,16 +1,16 @@
 <template>
 
-  <div>
-
-  <!--<el-radio-group v-model="labelPosition" size="small">
+  <div class='uesr_info_page'>
+<center>
+  <!-- <el-radio-group v-model="labelPosition" size="small">
     <el-radio-button label="left">左对齐</el-radio-button>
     <el-radio-button label="right">右对齐</el-radio-button>
     <el-radio-button label="top">顶部对齐</el-radio-button>
-  </el-radio-group>-->
-<div style="margin: 20px;"></div>
-<el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+  </el-radio-group> -->
+<div style="margin: 50px;"></div>
+<el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign" style="width:40%;text-align:center" >
   <h2 class="title">个人信息</h2>
-  <el-form-item label="昵称">
+  <el-form-item label="昵称" prop="desc" class="descClass">
     <el-input v-model="formLabelAlign.name" readonly="true"></el-input>
   </el-form-item>
   <el-form-item label="手机号">
@@ -28,14 +28,15 @@
 
   <el-form-item class="button-item">
     <router-link to="/Revising">
-      <el-button type="primary" style="width:36%;" round>修 改 个 人 信 息</el-button>
+      <el-button type="primary" style="width:50%;" round>修 改 个 人 信 息</el-button>
     </router-link>
     <router-link to="/">
-      <el-button type="primary" style="width:36%;" round>返 回 主 页</el-button>
+      <el-button type="primary" style="width:50%;" round>返 回 主 页</el-button>
     </router-link>
   </el-form-item>
 
  </el-form>
+ </center>
   </div>
 </template>
 
@@ -48,7 +49,7 @@ export default {
     data() {
       
       return {
-        labelPosition: 'top',
+        labelPosition: 'right',
         
         formLabelAlign: {
           name: '',
