cc2d483d29b7f860aeac1306d631e09ee63d861b
TS <59762013+TsurukawaSalt@users.noreply.github.com>
2020-08-13 23:25:13 +0800

    优化了登陆状态丢失（可能

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -3,12 +3,16 @@ import vuex from 'vuex'
 
 import user from './modules/user'
 
+import createPersistedPlugin from "@/store/createPersistedPlugin";
+const persistedPlugin = createPersistedPlugin()
+
 Vue.use(vuex)
 
 const store = new vuex.Store({
   modules: {
     user: user
-  }
+  },
+  plugins: [persistedPlugin]
 })
 
 export default store
