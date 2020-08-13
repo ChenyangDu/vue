3de8536a83849a94beefa9b358ac2e0e6882cde5
TS <59762013+TsurukawaSalt@users.noreply.github.com>
2020-08-13 14:58:40 +0800

    再次恢复

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import global from '@/utils/global'
-import './plugins/element.js'
 import api from './http'
 import axios from 'axios'
 
