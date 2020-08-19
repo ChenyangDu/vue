e293c3f37d1107bb4c6cbbb83548d18fbad2b83b
unknown <568385476@qq.com>
2020-08-19 19:02:52 +0800

    修改了welcome

--- vue/src/views/Welcome.vue
+++ vue/src/views/Welcome.vue
@@ -1,36 +1,39 @@
 <template>
   <div class="hello">
-    <el-image src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" style="width: 200px; height: 200px" ></el-image>
-    <h1>欢迎使用金刚石文档</h1>
+    <el-image :src="imagepath" :lazy='true' style="width: 200px; height: 200px" ></el-image>
+    <h1>欢迎使用库里南文档</h1>
     <p>
-      如果您需要帮助，请点击右侧的帮助文档
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">帮助</a>.
+      库利南，面向未来的办公软件
+      <!-- <a href="https://cli.vuejs.org" target="_blank" rel="noopener">帮助</a>. -->
     </p>
+    <p>文档实时协同</p>
+    <p>知识沉淀管理</p>
+    <p>数据安全可控</p>
 
     <p>
       还没有账号？
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">立即注册</a>.
+      <a href="./#/register" target="_blank" rel="noopener">立即注册</a>.
     </p>
     <p>
       已有账号？
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">立即登录</a>.
+      <a href="./#/login" target="_blank" rel="noopener">立即登录</a>.
     </p>
     
-    <h3>想了解更多？</h3>
+    <!-- <h3>想了解更多？</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">核心文档</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">论坛</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">社区</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">微信公众号</a></li>
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">微博</a></li>
-    </ul>
+    </ul> -->
     <h3>关于我们</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">公司</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">开发团队</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">成员信息</a></li>
+      <li><a href="https://github.com/ChenyangDu/vue" target="_blank" rel="noopener">前端</a></li>
+      <li><a href="https://github.com/ChenyangDu/springboot" target="_blank" rel="noopener">后端</a></li>
+      <!-- <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">成员信息</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">意见反馈</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">联系方式</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">联系方式</a></li> -->
     </ul>
   </div>
 </template>
@@ -39,9 +42,12 @@
 
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data:function(){
+    return {
+      imagepath:'http://39.101.200.9:8081/image/system?id=diamond.png'
+    }
   }
+
 }
 </script>
 
